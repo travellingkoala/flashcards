e7ccc75978f4826e885c53d387588375934153fd
travellingkoala <travellingkoalaspanish@gmail.com>
2025-07-03 17:01:51 +1000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+
+const flashcardData = {
+  "Beginner A": [
+    ["Hola", "Hello"],
+    ["¿Cómo te llamas?", "What’s your name?"],
+    ["Me llamo Ana", "My name is Ana"],
+    ["¿De dónde eres?", "Where are you from?"],
+    ["Soy de México", "I’m from Mexico"]
+  ],
+  "Beginner B": [
+    ["Tengo hambre", "I’m hungry"],
+    ["¿Qué hora es?", "What time is it?"],
+    ["Voy al mercado", "I’m going to the market"],
+    ["Hace calor", "It’s hot"],
+    ["Está lloviendo", "It’s raining"]
+  ]
+};
